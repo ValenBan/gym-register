c45de4edade375147fc38ebd1df81bf222d6a7b3
Valentin Banegas <valenkkk@outlook.es>
2024-08-03 00:49:31 -0300

    User entity added

---
+++ gym-register/gym-register.Domain/Entity/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gym_register.Domain.Entity
+{
+    public class User
+    {
+        
+    }
+}
